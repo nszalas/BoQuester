a9d00b6e6b8bbd2cef9bf64388c618f2f911bacc
antobrz <antobrz642@student.polsl.pl>
2021-08-21 10:46:22 +0200

    added book repository and entities

---
+++ WpfApp1/DAL/Entities/BookUser.cs
@@ -0,0 +1,29 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.DAL.Entities
+{
+    class BookUser
+    {
+        public sbyte BooksUsersId { get; set; }
+        public sbyte UserId { get; set; }
+        public sbyte BookId { get; set; }
+        public bool IsRead { get; set; }
+        public bool WantToRead { get; set; }
+        public int Rate { get; set; }
+
+        public BookUser(MySqlDataReader reader)
+        {
+            BooksUsersId = sbyte.Parse(reader["books_users_id"].ToString());
+            UserId = sbyte.Parse(reader["user_id"].ToString());
+            BookId = sbyte.Parse(reader["book_id"].ToString());
+            IsRead = bool.Parse(reader["is_read"].ToString());
+            WantToRead = bool.Parse(reader["want_to_read"].ToString());
+            Rate = int.Parse(reader["rate"].ToString());
+        }
+    }
+}
---
+++ WpfApp1/DAL/Repositories/BookRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1.DAL.Repositories
+{
+    class BookRepository
+    {
+        #region QUERIES
+        private const string ADD_BOOK = "INSERT INTO `book`(`title`, `release_date`, `publisher`, `category`, `description`, `rate`) VALUES ";
+        private const string ALL_BOOKS = "SELECT * FROM book";
+        private const string UPDATE_BOOK = "";
+        private const string DELETE_BOOK = "DELETE FROM book WHERE book_id=...";
+        #endregion
+
+        #region metody CRUD 
+        /// <summary>
+        /// CRUD - create, read, update, delete
+        /// </summary>
+        /// <returns></returns>
+        #endregion
+
+
+    }
+}
